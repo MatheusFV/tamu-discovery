e6c7abe7e5e8563c6ee8e445cd93c53b65287caf
MatheusFV <matheus.venosa@usp.br>
2018-11-22 15:23:41 -0200

    polling fixes

--- modules/discovery/discoveryController.js
+++ modules/discovery/discoveryController.js
@@ -1,18 +1,25 @@
 var bonjour = require('bonjour')()
+const request = require('request');
+
+bonjour.publish({ name: 'DIBUA_2', type: 'http', port: 3001 })
+bonjour.publish({ name: 'DIBUA_1', type: 'http', port: 3002 })
 
 module.exports = {
-    discoverDevices(req, res) {
+    discoverDevices() {
         // Uncomment this to test
-        // bonjour.publish({ name: 'DIBUA_2', type: 'http', port: 3001 })
-        // bonjour.publish({ name: 'DIBUA_1', type: 'http', port: 3002 })
         var devices = []
         bonjour.find({ type: 'http' }, function (service) {
             if (service && service.name.indexOf("DIBUA") !== -1) {
-                devices.push({port: service.port, name: service.name})
+                devices.push(JSON.stringify({port: service.port, name: service.name, service: service}))
             }
         })
+
         setTimeout(function () {
-            res.json({devices: devices})
-        }, 4000, 'funky');
+            console.log(devices)
+            request.post('https://tamu-backend.herokuapp.com/message/savePorts', {devices}, function(err, response) {
+                console.log(err)
+                console.log(response)
+            })
+        }, 5000, 'funky');
     }
 }
--- modules/polling.js
+++ modules/polling.js
@@ -1,12 +1,12 @@
 const https = require('https');
-
+const discovery = require('./discovery/discoveryController');
 
 module.exports = {
     requestData() {
         https.get('https://tamu-backend.herokuapp.com/message/getMessages', (resp) => {
             // A chunk of data has been recieved.
             resp.on('data', (chunk) => {
-                console.log('Data received:', JSON.parse(chunk))
+                // console.log('Data received:', JSON.parse(chunk))
                 // Send the message to dibua
             });
 
@@ -16,5 +16,8 @@ module.exports = {
     },
     start() {
         setInterval(this.requestData, 3000);
+    },
+    refreshDevices() {
+        setInterval(discovery.discoverDevices, 5000);
     }
 }
